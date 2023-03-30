c4eab3981ea8359641e05b4e5431db4df90c7221
roisa <roisaalfarizi03092003@gmail.com>
2023-03-31 05:52:07 +0700

    backup

--- app/src/main/java/com/example/combinecolor/MainActivity.kt
+++ app/src/main/java/com/example/combinecolor/MainActivity.kt
@@ -25,46 +25,62 @@ class MainActivity : AppCompatActivity() {
         }else if(TextUtils.isDigitsOnly(warna1)){
             Toast.makeText(this, R.string.inputNumber,Toast.LENGTH_LONG).show()
             return
+        }else if (warna1 != "Merah" && warna1 != "Kuning" && warna1 != "Hijau" && warna1 != "Ungu" && warna1 != "Biru"){
+            Toast.makeText(this, R.string.outStock,Toast.LENGTH_LONG).show()
+            return
         }
 
-
-
         val warna2 = binding.Warna2.text.toString()
-        if(TextUtils.isEmpty(warna1)){
+        if(TextUtils.isEmpty(warna2)){
             Toast.makeText(this, R.string.Warna2_invalid,Toast.LENGTH_LONG).show()
             return
         }else if(TextUtils.isDigitsOnly(warna1)){
             Toast.makeText(this, R.string.inputNumber,Toast.LENGTH_LONG).show()
             return
         }
-        else if(TextUtils.isDigitsOnly(warna1)){
-            Toast.makeText(this, R.string.inputNumber,Toast.LENGTH_LONG).show()
+         else if ( warna2 != "Merah" && warna2 != "Kuning" && warna2 != "Hijau" && warna2 != "Ungu" && warna2 != "Biru"
+        ){
+            Toast.makeText(this, R.string.outStock,Toast.LENGTH_LONG).show()
             return
-        } else if (warna1 != "Merah" || warna1 != "Kuning" || warna1 != "Hijau" || warna1 != "Ungu" || warna1 != "Biru" ){
-            binding.Hasil.text = getString(R.string.warnaEmpty1)
 
 
-        }else if (warna2 != "Merah" || warna2 != "Kuning" || warna2 != "Hijau" || warna2 != "Ungu" || warna2 != "Biru" ){
-            binding.Hasil.text = getString(R.string.warnaEmpty2)
-
+        }else if(warna1 == "Merah" && warna2 == "Kuning"|| warna1 == "Kuning" && warna2 == "Merah"){
+            binding.Hasil.text = getString(R.string.Oren)
         }else if(warna1 == "Merah" && warna2 == "Hijau"|| warna1 == "Hijau" && warna2 == "Merah"){
           binding.Hasil.text = getString(R.string.Coklat)
       }else if(warna1 == "Merah" && warna2 == "Biru"|| warna1 == "Biru" && warna2 == "Merah"){
           binding.Hasil.text = getString(R.string.Ungu)
       }else if(warna1 == "Merah" && warna2 == "Ungu"|| warna1 == "Ungu" && warna2 == "Merah"){
           binding.Hasil.text = getString(R.string.Pink)
-      }else if(warna1 == "Kuning" && warna2 == "Hijau"|| warna1 == "Hijau" && warna2 == "Kuning"){
+      }else if(warna1 == "Merah" && warna2 == "Merah"){
+            binding.Hasil.text = getString(R.string.Merah)
+        }
+
+
+
+        else if(warna1 == "Kuning" && warna2 == "Hijau"|| warna1 == "Hijau" && warna2 == "Kuning"){
           binding.Hasil.text = getString(R.string.Hijau_muda)
       }else if(warna1 == "Kuning" && warna2 == "Biru"|| warna1 == "Biru" && warna2 == "Kuning"){
           binding.Hasil.text = getString(R.string.Hijau)
       }else if(warna1 == "Kuning" && warna2 == "Ungu"|| warna1 == "Ungu" && warna2 == "Kuning"){
           binding.Hasil.text = getString(R.string.Krem)
-      }else if(warna1 == "Hijau" && warna2 == "Biru"|| warna1 == "Biru" && warna2 == "Hijau"){
+      }else if(warna1 == "Kuning" && warna2 == "Kuning"){
+            binding.Hasil.text = getString(R.string.Kuning)
+        }
+
+
+        else if(warna1 == "Hijau" && warna2 == "Biru"|| warna1 == "Biru" && warna2 == "Hijau"){
           binding.Hasil.text = getString(R.string.Hijau_muda)
       }else if(warna1 == "Hijau" && warna2 == "Ungu"|| warna1 == "Ungu" && warna2 == "Hijau"){
           binding.Hasil.text = getString(R.string.Abu)
-      }else if(warna1 == "Biru" && warna2 == "Ungu"|| warna1 == "Ungu" && warna2 == "Biru"){
+      }else if(warna1 == "Hijau" && warna2 == "Hijau"){
+            binding.Hasil.text = getString(R.string.Hijau)
+        }
+
+        else if(warna1 == "Biru" && warna2 == "Ungu"|| warna1 == "Ungu" && warna2 == "Biru"){
           binding.Hasil.text = getString(R.string.Biru_tua)
-      }
+      }else if(warna1 == "Biru" && warna2 == "Biru"){
+            binding.Hasil.text = getString(R.string.Biru)
+        }
     }
 }
